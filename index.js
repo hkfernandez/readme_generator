f849467a1f54d37d04129be0216f9238e62a23bc
Hector Fernandez <hkfernandezdev@gmail.com>
2021-01-25 20:52:30 -0600

    initial prompt workinng

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 // TODO: Include packages needed for this application
+const inquirer = require ('inquirer');
+const fs = require ('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['Project Title:', 'PRoject Description', 'Installation Instructions', 'Usage - how the app is used', "Contributors", "Testing Instructions"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+// function writeToFile(fileName, data) {}
+function test () {
+      inquirer.prompt({
+            type: 'input',
+            message: questions[i],
+            name: 'userInput',
+      })
+      .then (() => console.log("working"));
+            
+}
+test ();
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: 'What is your user name?',
+//       name: 'username',
+//     },
+//     {
+//       type: 'password',
+//       message: 'What is your password?',
+//       name: 'password',
+//     },
+//     {
+//       type: 'password',
+//       message: 'Re-enter password to confirm:',
+//       name: 'confirm',
+//     },
+//   ])
+//   .then((response) =>
+//     response.confirm === response.password
+//       ? console.log('Success!')
+//       : console.log('You forgot your password already?!')
+//   );
